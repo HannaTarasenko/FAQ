0d201db5489f7237265fffa99a7f85ed49bfee3c
Hanna <tarasenko.ganna.1998@gmail.com>
2024-07-03 23:26:13 +0100

    add html css

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     const items = document.querySelectorAll('.question-title');
+
+//     items.forEach(item => {
+//         item.addEventListener('click', function () {
+//             this.parentNode.querySelector('.arrow-button').classList.toggle('arrow-up');
+//             this.parentNode.nextElementSibling.classList.toggle('show-answer');
+//         });
+//     });
+// });
